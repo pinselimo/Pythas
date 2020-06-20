68d625fb5e84bd88c674bb04a51eda961e8519a8
Simon Plakolb <s.plakolb@gmail.com>
2020-06-21 00:00:17 +0200

    Remove unnecessary haskell parsing

--- hasky/parser.py
+++ hasky/parser.py
@@ -2,4 +2,4 @@
 from collections import namedtuple
 
 ParseInfo = namedtuple('ParseInfo',['name','dir','exported_mod','exported_ffi','excluded','func_infos'])
-FuncInfo = namedtuple('FuncInfo',['name','functype','exporttype','argtypes','restype','constructors','reconstructor','destructor', 'destr_type','native_from_c','native_to_c'])
+FuncInfo = namedtuple('FuncInfo',['name','argtypes','restype','constructors','reconstructor'])
