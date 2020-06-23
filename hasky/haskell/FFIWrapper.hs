773dad694bb4d0aace010636fad73d9d59bd7e9b
Simon Plakolb <s.plakolb@gmail.com>
2020-06-23 09:25:45 +0200

    Improve tuple finalizing to unpack tuples

--- hasky/haskell/FFIWrapper.hs
+++ hasky/haskell/FFIWrapper.hs
@@ -80,34 +80,35 @@ wrapRes' cv maps res = case cv of
     (Pure (ToC cv))    -> "(return . " ++ putMaps Map maps ++ ' ':cv++res++")"
     (Nested a b _)     -> wrapRes' a maps "" ++ " =<< " ++ wrapRes' b (maps+1) res
     (Tuple2 a b)       -> putMaps Map maps ++ "(\\(a,b) -> liftM2 toTuple2 " ++ wrapRes' a 0 " a" ++ ' ':wrapRes' b 0 " b"  ++ ")" ++ res
-    (Tuple3 a b c)     -> undefined
+    (Tuple3 a b c)     -> putMaps Map maps ++ "(\\(a,b,c) -> liftM3 toTuple3 " ++ wrapRes' a 0 " a" ++ ' ':wrapRes' b 0 " b" ++ ' ':wrapRes' c 0 " c" ++ ")" ++ res
 
-finalizerFunc :: String -> Convert -> HType -> String
-finalizerFunc n freer ft = needsFinalizer freer $ finalizerName n ++ " x = " ++ finalizerFunc' freer 0 ft " x" ++ "\n"
 
-finalizerFunc' :: Convert -> Int -> HType -> String -> String
+finalizerFunc :: String -> Convert -> String
+finalizerFunc n freer = needsFinalizer freer $ finalizerName n ++ " x = " ++ finalizerFunc' freer 0 " x" ++ "\n"
+
+finalizerFunc' :: Convert -> Int -> String -> String
 finalizerFunc' = finalizerFunc'' [""]
 
-finalizerFunc'' :: [String] -> Convert -> Int -> HType -> String -> String
-finalizerFunc'' peek cv maps ft var =
+finalizerFunc'' :: [String] -> Convert -> Int -> String -> String
+finalizerFunc'' peek cv maps var =
     let tuple2access = ["c2fst","c2snd"]
         tuple3access = ["c3fst","c3snd","c3trd"]
-        nestedff x = finalizerFunc'' peek x maps ft var
+        nestedff x = finalizerFunc'' peek x maps var
     in case cv of
-    Tuple2 a b          -> finalizeTuple tuple2access peek [a,b] maps ft var
-    Tuple3 a b c        -> finalizeTuple tuple3access peek [a,b,c] maps ft var
+    Tuple2 a b          -> finalizeTuple tuple2access peek [a,b] maps var
+    Tuple3 a b c        -> finalizeTuple tuple3access peek [a,b,c] maps var
     Nested a (Pure _) p -> nestedff a
-    Nested a b p        -> finalizerFunc'' (p:peek) b (maps+1) ft var ++ plus ++ nestedff a
+    Nested a b p        -> finalizerFunc'' (p:peek) b (maps+1) var ++ plus ++ nestedff a
     IOOut (Free f) _    -> if maps > 0
                      then '(':'(':putMaps MapM maps ++ ' ':f++')':get maps peek var
                      else '(':putMaps MapM maps ++ ' ':f++var++")"
 
-finalizeTuple :: [String] -> [String] -> [Convert] -> Int -> HType -> String -> String
-finalizeTuple access peek cvs maps ft var = case fs of
+finalizeTuple :: [String] -> [String] -> [Convert] -> Int -> String -> String
+finalizeTuple access peek cvs maps var = case fs of
                  []     -> ""
                  (x:[]) -> x
                  _      -> foldr (\a b-> a ++ plus ++ b) (last fs) (init fs)
-    where tupleff p x = finalizerFunc'' (p:peek) x maps ft var
+    where tupleff p x = '(':finalizerFunc'' peek x maps "" ++ " $ " ++ p ++ var ++ ")"
           fs = map (uncurry tupleff) $ filter (\(_,a)->needsFinalizer' a) $ zip access cvs
 
 get :: Int -> [String] -> String -> String
