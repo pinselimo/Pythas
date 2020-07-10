023de4fcdeb1fb08b772fdb2b7fc42ced3f0fab5
Simon Plakolb <s.plakolb@gmail.com>
2020-07-11 00:35:15 +0200

    Add prototype for finalizing

--- hasky/haskell/FFIFinalizer.hs
+++ hasky/haskell/FFIFinalizer.hs
@@ -1,27 +1,45 @@
 module FFIFinalizer where
 
+import Control.Monad (liftM2, liftM)
+
 import HTypes (HType(..))
 import ParseTypes (TypeDef(funcN, funcT))
 import FFIUtils
+import FFIWrapper (wrapFunc)
 
-maybeFinalizerFunc :: HType -> Maybe HAST
-maybeFinalizerFunc ht = let finType = toFFIType' ht in
-    if needsFinalizer finType
-    then Just $ mkFinalizer finType (Variable "x" finType)
-    else Nothing
+maybeFinalizerFunc :: String -> HType -> Maybe String
+maybeFinalizerFunc n ht = case ht of
+    HIO t -> f t
+    t     -> f t
+    where mkFinalizer h = (finalizerName n) ++ " x = " ++ show h
+          f t = liftM mkFinalizer $ maybeFinalizerFunc' t
 
-mkFinalizer :: HType -> HAST -> HAST
-mkFinalizer ht hast = case ht of
-    HCWString   -> Function "freeCWString" [hast] (HIO HUnit)
-    HCArray ht' -> Next (map' $ mkFinalizer ht' hast) $ Function "freeArray" [hast] (HIO HUnit)
+maybeFinalizerFunc' :: HType -> Maybe HAST
+maybeFinalizerFunc' ht = let finType = stripIO $ toFFIType' ht in
+    finalize finType (Variable "x" finType)
+
+finalize :: HType -> HAST -> Maybe HAST
+finalize ht hast = case ht of
+      HCArray a -> freeArray a hast
+      _         -> free' ht hast
+
+freeArray :: HType -> HAST -> Maybe HAST
+freeArray ht hast = let
+    inner = case ht of
+        HCArray a -> freeArray a hast
+        _         -> free' ht hast
+    in liftM2 Next (inner >>= (wrap' $ HList ht)) $ free' (HCArray ht) hast
+
+free' :: HType -> HAST -> Maybe HAST
+free' ht hast = case ht of
+    HCWString   -> Just $ f "freeCWString"
+    HCArray _   -> Just $ f "freeArray"
     HCTuple _   -> undefined
-    HCPtr   _   -> Function "free" [hast] (HIO HUnit)
-    _           -> id' ht
+    HCPtr   _   -> Just $ f "free"
+    _           -> Nothing
+    where f n = Function n [hast] $ HIO HUnit
 
-needsFinalizer :: HType -> Bool
-needsFinalizer ht = case ht of
-    HCArray _ -> True
-    HCWString -> True
-    HCTuple _ -> True
-    HCPtr _   -> True
-    _         -> False
+wrap' :: HType -> HAST -> Maybe HAST
+wrap' ht hast = case hast of
+    Function n _ t -> Just $ wrapFunc n [ht,t] [hast]
+    _              -> Nothing
