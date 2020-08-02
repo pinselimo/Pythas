b1cf3be9239adfa03fc97ac86c6b8f0ca50dec63
Simon Plakolb <s.plakolb@gmail.com>
2020-08-02 21:41:37 +0200

    Clean up namespace

--- pythas/__init__.py
+++ pythas/__init__.py
@@ -2,11 +2,7 @@ from .utils import check_has_ghc
 check_has_ghc()
 
 from .core import install
-from .context import Context
-from .haskell import GHC, ffi_creator
-
-ghc = GHC()
-context = Context(ffi_creator, ghc)
+from .context import context
 
 install(context)
 
--- pythas/context.py
+++ pythas/context.py
@@ -1,13 +1,24 @@
 import tempfile
 from ctypes import cdll
+from .haskell import GHC, ffi_creator
 
 from .utils import get_shared_library_suffix, remove_created_files
 from .haskell.parse_file import parse_haskell
 
 class Context:
-    def __init__(self, ffi_creator, compiler):
+    def __init__(self):
         self.__fficreator = ffi_creator
-        self.__compiler = compiler
+        self.__compiler = GHC()
+
+    @property
+    def flags(self):
+        return self.__compiler.custom_flags()
+
+    def add_flag(self, flag):
+        self.__compiler.custom_flags.add_flag(flag)
+
+    def remove_flag(self, flag):
+        self.__compiler.custom_flags.remove_flag(flag)
 
     @property
     def compiler(self):
@@ -23,4 +34,6 @@ class Context:
             remove_created_files(ffi_filename)
 
         return lib, ffi_pinfos
+
+context = Context()
 
