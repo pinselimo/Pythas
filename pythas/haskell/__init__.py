b28ce5b96ffb9d7ef27a340df87e69fbc87b4967
Simon Plakolb <s.plakolb@gmail.com>
2020-08-02 20:32:44 +0200

    Improve compile logic

--- pythas/haskell/__init__.py
+++ pythas/haskell/__init__.py
@@ -1,10 +1,10 @@
 import os.path
 from ctypes import cdll, c_wchar_p, c_voidp
-from subprocess import run
 from sys import platform
 
-from .ghc import ghc_compile_cmd
+from .ghc import GHC
 
+ghc = GHC()
 dir = os.path.dirname(os.path.realpath(__file__))
 dir = os.path.join( dir, "ffi", "src" )
 src = os.path.join( dir, "Exports.hs")
@@ -14,12 +14,14 @@ if not os.path.exists( lib ):
         lib += '.so'
     elif platform.startswith('win32'):
         lib += '.dll'
-    cmd = ghc_compile_cmd(src,lib,dir,platform,redirect=True)
-    run(cmd)
 
-hsparser = cdll.LoadLibrary( lib )
-hsparser.createFileBindings.argtypes = [c_wchar_p]
-hsparser.createFileBindings.restype = c_wchar_p
-hsparser.freeReturnedString.argtypes = [c_wchar_p]
-hsparser.freeReturnedString.restype = c_voidp
+ghc.compile(src, lib, redirect=True)
+
+ffi_creator = cdll.LoadLibrary( lib )
+ffi_creator.newFileBindings.argtypes = [c_wchar_p, c_wchar_p]
+ffi_creator.newFileBindings.argtypes = [c_wchar_p, c_wchar_p]
+ffi_creator.createFileBindings.argtype = [c_wchar_p]
+ffi_creator.createFileBindings.restype = c_wchar_p
+ffi_creator.freeReturnedString.argtypes = [c_wchar_p]
+ffi_creator.freeReturnedString.restype = c_voidp
 
