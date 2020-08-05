ffb1020a4afb760c02193a19e21e7debbec061ab
Simon Plakolb <s.plakolb@gmail.com>
2020-08-05 11:05:05 +0200

    Add testing function for SourceModule

--- test/test_interface.py
+++ test/test_interface.py
@@ -1,6 +1,11 @@
 from .context import pythas
 import test.hs.testcases as t
 
+from hypothesis import strategies, given
+
+strings = strategies.text()
+tuples  = strategies.tuples
+
 def test_dynamic_dir():
     init_dir = set(dir(t))
     t.something = 63
@@ -17,4 +22,14 @@ def test_sourcemodules():
             ''')
     assert m.i == 63
     assert m.f([('a','b'),('c','d')]) == 2
+
+@given(tuples(strings,strings), strings)
+def test_compilerflags(t,s):
+    pythas.compiler.add_flag(t)
+    pythas.compiler.add_flag(s)
+    assert pythas.compiler.flags == t + (s,)
+    pythas.compiler.remove_flag(t)
+    assert pythas.compiler.flags == (s,)
+    pythas.compiler.remove_flag(s)
+    assert pythas.compiler.flags == ()
 
