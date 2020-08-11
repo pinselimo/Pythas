5020bd253ee8a5901f200d54858d933c46f90cec
Simon Plakolb <s.plakolb@gmail.com>
2020-08-11 17:38:42 +0200

    Minor refactoring

--- pythas/haskell/__init__.py
+++ pythas/haskell/__init__.py
@@ -3,19 +3,20 @@ from ctypes import cdll, c_wchar_p, c_voidp
 from sys import platform
 
 from .ghc import GHC
+from ..utils import shared_library_suffix
 
-ghc = GHC()
-dir = os.path.dirname(os.path.realpath(__file__))
-dir = os.path.join( dir, "ffi", "src" )
-src = os.path.join( dir, "Exports.hs")
-lib = os.path.join( dir, "libpythasffi")
+dir = os.path.join(
+          os.path.dirname(os.path.realpath(__file__))
+        , 'ffi'
+        , 'src'
+        )
+
+src = os.path.join(dir, "Exports.hs")
+lib = os.path.join(dir, "libpythasffi")
 if not os.path.exists( lib ):
-    if platform.startswith('linux'):
-        lib += '.so'
-    elif platform.startswith('win32'):
-        lib += '.dll'
+    lib += shared_library_suffix()
 
-ghc.compile(src, lib, redirect=True)
+GHC().compile(src, lib, redirect=True)
 
 ffi_creator = cdll.LoadLibrary( lib )
 ffi_creator.createFileBindings.argtype = [c_wchar_p]
