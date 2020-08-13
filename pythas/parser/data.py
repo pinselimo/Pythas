a9fe35dcb03751037a3bd754534cc7d90902b13d
Simon Plakolb <s.plakolb@gmail.com>
2020-08-13 13:42:19 +0200

    Minor refactor

--- pythas/parser/data.py
+++ pythas/parser/data.py
@@ -1,5 +1,9 @@
-
 from collections import namedtuple
 
-ParseInfo = namedtuple('ParseInfo',['name','dir','exported_mod','exported_ffi','func_infos'])
-FuncInfo = namedtuple('FuncInfo',['name','argtypes','restype','constructors','reconstructor','htype'])
+ParseInfo = namedtuple('ParseInfo',[
+    'name','dir','exported_mod','exported_ffi','func_infos'
+    ])
+FuncInfo = namedtuple('FuncInfo',[
+    'name','argtypes','restype','constructors','reconstructor','htype'
+    ])
+
