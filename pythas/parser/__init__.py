8ee7ad568cf4bac0414bb61641cc50de671e33bc
Simon Plakolb <s.plakolb@gmail.com>
2020-12-31 22:10:09 +0100

    Add description for parser module

--- pythas/parser/__init__.py
+++ pythas/parser/__init__.py
@@ -1,4 +1,12 @@
-"""Init file of parser package."""
+"""
+The parser module provides the minimal parsing capability necessary
+to derive the types of the imported functions and constants. Note that
+the parser responsible for parsing the original Haskell source is
+implemented in Haskell. It can be found in the haskell module's ffi directory.
+
+The parser within this module is strictly limited to read "foreign export ccall"
+type annotations.
+"""
 
 from .parse_file import parse_haskell
 
