0ee8038f239217a7b783423cb2b8cdc92aab8bf1
Simon Plakolb <s.plakolb@gmail.com>
2020-12-31 22:10:40 +0100

    Make first line in pythas doc bold

--- pythas/__init__.py
+++ pythas/__init__.py
@@ -1,12 +1,13 @@
 """
-Pythonically import Haskell modules into your Python context.
+**Pythonically import Haskell modules into your Python context.**
 
 Pythas will automatically find a Haskell module and parse it. It will then create a module containing calls to the Haskell FFI wrapping everything compatible in the imported module. These calls are subsequently compiled into a shared library and parsed by the Python part of Pythas. Ultimately, Pythas imports this shared library as a module-like object into Python and wraps the function calls accordingly. In effect, for the user the calls to the Haskell runtime are almost indistinguishable to common Python calls.
 
 Submodules
-----------
+^^^^^^^^^^
 
 haskell : Containing all source written in Haskell and a wrapper around the GHC instance.
+
 parser : A basic parser for exported Haskell type declarations.
 
 """
