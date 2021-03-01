80975f04a70b1ba754e246ae18fa9203e97273fc
Simon Plakolb <s.plakolb@gmail.com>
2021-03-01 15:25:04 +0100

    Update __version__ string

--- pythas/__init__.py
+++ pythas/__init__.py
@@ -12,7 +12,7 @@ parser : A basic parser for exported Haskell type declarations.
 
 """
 
-__version__ = "0.01dev"
+__version__ = "0.01"
 __author__ = "Simon Plakolb"
 __email__ = "s.plakolb@gmail.com"
 __copyright__ = "Copyright 2020, Simon Plakolb"
