114d7b7bea0c8fc4e29da77e7fe02e5c92360f32
Simon Plakolb <s.plakolb@gmail.com>
2021-03-01 15:18:08 +0100

    Run black formatting on pythas test source

--- test/test_interface.py
+++ test/test_interface.py
@@ -3,35 +3,42 @@ from .context import pythas
 from hypothesis import strategies, given
 
 strings = strategies.text()
-tuples  = strategies.tuples
+tuples = strategies.tuples
+
 
 def test_dynamic_dir():
     import test.hs.testcases as t
+
     init_dir = set(dir(t))
     t.something = 63
-    new_dir  = set(dir(t))
-    assert new_dir - init_dir == {'something'}
+    new_dir = set(dir(t))
+    assert new_dir - init_dir == {"something"}
+
 
 def test_sourcemodules():
-    m = pythas.SourceModule('''
+    m = pythas.SourceModule(
+        """
             i :: Int
             i = 63
 
             f :: [(String, String)] -> Int
             f = length
-            '''
-            , compiler = pythas.compiler
-            )
+            """,
+        compiler=pythas.compiler,
+    )
     assert m.i == 63
-    assert m.f([('a','b'),('c','d')]) == 2
+    assert m.f([("a", "b"), ("c", "d")]) == 2
+
 
 def test_stack_switch():
     pythas.compiler.stack_usage = False
     import test.hs.testcases as t
+
     assert t.constantInt == 63
 
-@given(tuples(strings,strings), strings)
-def test_compilerflags(t,s):
+
+@given(tuples(strings, strings), strings)
+def test_compilerflags(t, s):
     base_flags = pythas.compiler.flags
     pythas.compiler.add_flag(t)
     pythas.compiler.add_flag(s)
@@ -40,4 +47,3 @@ def test_compilerflags(t,s):
     assert pythas.compiler.flags == base_flags + (s,)
     pythas.compiler.remove_flag(s)
     assert pythas.compiler.flags == base_flags
-
--- test/test_lists.py
+++ test/test_lists.py
@@ -10,37 +10,45 @@ utils = pythas.utils
 # Haskell import
 import test.hs.testlists as hst
 
+
 @given(lists(c_ints))
 def test_listInt(l):
     assert hst.listInt(l) == l
+
 
 @given(lists(c_doubles))
 def test_listDouble(l):
     assert hst.listDouble(l) == l
 
+
 @given(lists(c_chars))
 def test_listDouble(l):
     assert hst.listChar(l) == l
+
 
 @given(lists(c_strings))
 def test_listString(l):
     assert hst.listString(l) == l
 
+
 # Nested
+
 
 @given(lists(lists(c_ints)))
 def test_listListInt(l):
     assert hst.listListInt(l) == l
 
+
 @given(lists(lists(c_strings)))
 def test_listListString(l):
     assert hst.listListString(l) == l
 
-@given(lists(lists(tuples(c_strings,c_strings))))
+
+@given(lists(lists(tuples(c_strings, c_strings))))
 def test_listListTupleStringString(l):
     assert hst.listListTupleStringString(l) == l
+
 
 @given(lists(lists(lists(c_strings))))
 def test_listListListString(l):
     assert hst.listListListString(l) == l
-
