2963819f2084467a80f650362f55efd17b210d7a
Simon Plakolb <s.plakolb@gmail.com>
2021-03-29 13:24:53 +0200

    Use constants in setup.py

--- setup.py
+++ setup.py
@@ -1,39 +1,43 @@
 from setuptools import setup
 
-dirs = (
+DESC = "Import Haskell modules as if they were Python modules"
+
+DIRS = (
     ["res/cstructs-in-haskell/src/Foreign/C/" + s for s in ["", "Structs/"]]
     + ["res/Pythas-Types/src/Foreign/Pythas/", "res/", "ffi/src/", "bin/"]
     + ["ffi/src/Foreign/" + s for s in ["", "Pythas/"]]
     + ["bin/Foreign/" + s for s in ["", "C/", "Pythas/", "C/Structs/"]]
 )
 
-suffices = [".hs", ".hi", ".o", ".so", ".c", ".h"]
+SUFFICES = [".hs", ".hi", ".o", ".so", ".c", ".h"]
 
 with open("README.md", "r") as f:
-    long_description = f.read()
+    LONG_DESCRIPTION = f.read()
 
-with open("requirements.txt") as f:
-    requirements_lines = f.readlines()
-install_requires = [r.strip() for r in requirements_lines]
-
-import pythas
+if os.getenv("READTHEDOCS") == "True":
+    INSTALL_REQUIRES = []
+else:
+    with open("requirements.txt") as f:
+        INSTALL_REQUIRES = [r.strip() for r in f.readlines()]
 
 setup(
-    name="Pythas",
-    version="0.01",
-    description="Import Haskell modules as if they were Python modules",
-    license="LGPLv3",
-    long_description=long_description,
+    name="pythas",
+    version="0.1.0",
+    description=DESC,
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     author="Simon Plakolb",
     author_email="s.plakolb@gmail.com",
-    url="https://www.behaviour.space/",
+    license="LGPLv3",
+    platforms="any",
+    url="https://github.com/pinselimo/Pythas",
     packages=["pythas", "pythas.parser", "pythas.haskell"],
     package_dir={
         "pythas": "pythas",
         "pythas.parser": "pythas/parser",
         "pythas.haskell": "pythas/haskell",
     },
-    package_data={"pythas.haskell": [d + "*" + s for d in dirs for s in suffices]},
-    install_requires=install_requires,
+    package_data={"pythas.haskell": [d + "*" + s for d in DIRS for s in SUFFICES]},
+    install_requires=INSTALL_REQUIRES,
+    python_requires=">=3.7",
 )
