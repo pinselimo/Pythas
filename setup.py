61f8ef29d04109cc151db6b822e91448ab817f4c
Simon Plakolb <s.plakolb@gmail.com>
2021-03-29 13:55:26 +0200

    Add list of pypi classifiers to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,29 @@ import os
 
 DESC = "Import Haskell modules as if they were Python modules"
 
+CLASSIFIERS = [
+    "Development Status :: 4 - Beta",
+    "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+    "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+    "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+    "Operating System :: OS Independent",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Education",
+    "Intended Audience :: Science/Research",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Haskell",
+    "Topic :: Scientific/Engineering :: Physics",
+    "Topic :: Scientific/Engineering :: Mathematics",
+    "Topic :: Scientific/Engineering :: Information Analysis",
+    "Topic :: Education",
+    "Topic :: Software Development",
+    "Typing :: Typed"
+    ]
+
 DIRS = (
     ["res/cstructs-in-haskell/src/Foreign/C/" + s for s in ["", "Structs/"]]
     + ["res/Pythas-Types/src/Foreign/Pythas/", "res/", "ffi/src/", "bin/"]
@@ -41,4 +64,5 @@ setup(
     package_data={"pythas.haskell": [d + "*" + s for d in DIRS for s in SUFFICES]},
     install_requires=INSTALL_REQUIRES,
     python_requires=">=3.7",
+    classifiers=CLASSIFIERS
 )
